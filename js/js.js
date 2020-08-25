e767d676ce8eecadd63f174ec326c931510945e6
Marco Andree Mazariegos <66434988+MarcoMazariegos@users.noreply.github.com>
2020-08-25 12:03:50 -0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+var botonLis4 = document.getElementById('menu')
+var boton4 = document.getElementById('menudes');
+
+function deplegarMenu() {
+  boton4.classList.add('mostrar');
+}
+
+boton4.addEventListener("mouseover", desplegarMenu, true);
