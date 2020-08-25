6b1919fe65ee6be5fafd55cca18ac4f86daaf3a9
Marco Andree Mazariegos <66434988+MarcoMazariegos@users.noreply.github.com>
2020-08-25 14:33:03 -0600

    Delete js.js

--- js/js.js
+++
@@ -1,8 +0,0 @@
-var botonLis4 = document.getElementById('menu')
-var boton4 = document.getElementById('menudes');
-
-function deplegarMenu() {
-  boton4.classList.add('mostrar');
-}
-
-boton4.addEventListener("mouseover", desplegarMenu, true);
